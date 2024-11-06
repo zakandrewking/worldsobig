a487bb5467a8c0f0dfc4249f40db023bcec92843
Zachary King <zak@delfina.com>
2024-11-06 10:19:14 -0800

    custom zoom buttons

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,12 +3,23 @@
 /**
  * 3d maps
  * https://developers.google.com/maps/documentation/javascript/3d-maps-getting-started
+ * https://visgl.github.io/react-google-maps/examples/map-3d
  */
 
 import { Scope_One } from "next/font/google";
 import React from "react";
 
 import { Map, useMap } from "@vis.gl/react-google-maps";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const scopeOne = Scope_One({ subsets: ["latin"], weight: "400" });
 
@@ -31,6 +42,25 @@ export default function Home() {
       panorama.setPov(pov);
     }
   }, [map]);
+  const [open, setOpen] = React.useState(true);
+  const [zoom, setZoom] = React.useState(13);
+
+  React.useEffect(() => {
+    if (map) {
+      map.setZoom(zoom);
+    }
+  }, [map, zoom]);
+
+  // React.useEffect(() => {
+  //   const listener = () => {
+  //     setOpen((o) => !o);
+  //   };
+  //   document.getElementById("map")!.addEventListener("click", listener);
+  //   return () => {
+  //     document.getElementById("map")!.removeEventListener("click", listener);
+  //   };
+  // }, []);
+
   return (
     <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
       <span
@@ -47,7 +77,31 @@ export default function Home() {
       >
         Hello World
       </span>
+      <Button
+        onClick={() => setZoom((z) => z + 1)}
+        className="fixed w-28 h-28 bottom-[130px] left-2 z-50"
+      >
+        In
+      </Button>
+      <Button
+        onClick={() => setZoom((z) => z - 1)}
+        className="fixed w-28 h-28 bottom-2 left-2 z-50"
+      >
+        Out
+      </Button>
+      <DropdownMenu open={open} onOpenChange={setOpen}>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
       <div
+        id="map"
         style={{
           width: "100%",
           height: "100%",
